e855d42c8fa70af68f8afb342ab7d14f8f40f90e
artyomshaitor <artyomshaitor@gmail.com>
2014-11-10 22:21:56 +0300

    First Commit

---
+++ src/main/java/com/onlineshop/controllers/MainController.java
@@ -0,0 +1,20 @@
+package com.onlineshop.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Artyom on 10.11.2014.
+ */
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String indexPage(ModelMap model) {
+
+        model.addAttribute("message", "Hello, World!");
+        return "index";
+    }
+}
